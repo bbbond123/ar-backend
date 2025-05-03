6ab62a1db285c4d7fd5758954082f5921b4e3bfe
yb <wangyibu123@gmail.com>
2025-05-04 04:25:09 +0900

    update code

--- main.go
+++ main.go
@@ -3,3 +3,13 @@
 // @description API for ar-backend
 // @BasePath /api/v1
 package main
+
+import "fmt"
+
+// @title Travel AR API
+// @version 1.0
+// @description API for Travel AR Project
+// @BasePath /api/v1
+func main() {
+	fmt.Println("Starting server...")
+}
