744ae310af5c329fc834d7cdddb3b75287ec7de4
yb <wangyibu123@gmail.com>
2025-05-04 09:41:11 +0900

    database config file

--- main.go
+++ main.go
@@ -1,7 +1,3 @@
-// @title My API
-// @version 1.0
-// @description API for ar-backend
-// @BasePath /api/v1
 package main
 
 import (
--- pkg/database.go
+++ pkg/database.go
@@ -1,8 +1,10 @@
 package database
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -10,7 +12,23 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "host=localhost user=myuser password=mypassword dbname=mydatabase port=5432 sslmode=disable TimeZone=Asia/Tokyo"
+	viper.SetConfigName("config") // config.yaml
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Config file error: %s", err)
+	}
+
+	host := viper.GetString("database.host")
+	user := viper.GetString("database.user")
+	password := viper.GetString("database.password")
+	dbname := viper.GetString("database.dbname")
+	port := viper.GetString("database.port")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo",
+		host, user, password, dbname, port)
+
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database!", err)
