4c208fbfea9b4cfd057e4017017f19e2653d69b0
yb <wangyibu123@gmail.com>
2025-05-21 03:28:57 +0900

    update code

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,16 +8,17 @@ export default defineConfig({
     proxy: {
       // 代理 /api 开头的请求到后端
       "/api": {
-        target: "http://localhost:3000",
+        // target: "http://localhost:3000",
+        target: "https://api.ifoodme.com/",
         changeOrigin: true,
         // 如果后端没有 /api 前缀，可以加上 rewrite
         // rewrite: (path) => path.replace(/^\/api/, ''),
       },
-      // 你也可以加上 /auth 代理
-      "/auth": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
+      // // 你也可以加上 /auth 代理
+      // "/auth": {
+      //   target: "http://localhost:3000",
+      //   changeOrigin: true,
+      // },
     },
   },
 });
