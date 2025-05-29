64b2b944b2f518eb2a8635bf251339fe540eed3e
yb <wangyibu123@gmail.com>
2025-05-30 01:24:05 +0900

    update environment configuration for development and production, ensuring API URL is correctly set for each environment

--- internal/service/routes.go
+++ internal/service/routes.go
@@ -142,8 +142,6 @@ func (s *Server) getAuthCallbackFunction(c *gin.Context) {
 		// 2. 根据环境判断默认地址
 		if os.Getenv("ENVIRONMENT") == "production" {
 			frontendURL = "https://www.ifoodme.com/"
-		} else {
-			frontendURL = "http://localhost:5173/"
 		}
 	}
 
