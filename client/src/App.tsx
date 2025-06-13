8a66ac5f0edf0dbabef8023f255a1ba8355c3585
yb <wangyibu123@gmail.com>
2025-06-14 04:17:21 +0900

    Remove unused logo links from App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -101,14 +101,6 @@ function App() {
             管理后台
           </a>
         </nav>
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
         <GoogleLoginButton />
       </div>
     );
